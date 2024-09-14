97a09d89cb256ae5da47b02f9b6b27b793118f45
JgSky1 <105887428+jg-ssj@users.noreply.github.com>
2024-09-14 15:26:29 -0600

    Respaldo

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ['https://hivesync-pixelr.vercel.app'],
-  methods: ['GET', 'POST'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-}));
+app.use(cors());
 
 app.get('/api/get-token', (req, res) => {
   try {
