bd4b73766bd4cf0fdb09a591aed8c7be62d6f1fb
JgSky1 <105887428+jg-ssj@users.noreply.github.com>
2024-09-13 22:35:23 -0600

    URLS updated

--- backend/jwtUtils.js
+++ backend/jwtUtils.js
@@ -5,7 +5,7 @@ export function generateJwtToken() {
   const payload = {
     sub: process.env.CLIENT_KEY,
     mode: 'embedded',
-    origin: 'https://tu-dominio.com',
+    origin: 'https://hivesync-pixelr.vercel.app/',
   };
 
   const token = jwt.sign(payload, process.env.CLIENT_SECRET, {
